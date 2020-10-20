e6bd70fbfc8a0b24e670bbc21e23cf3d62be0db2
MintoJ <jeff_alan@hotmail.com>
2020-10-20 16:53:52 +0100

    updated for webpack hot modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
+const path = require('path')
+
 const postCSSPlugins = [
-    require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require('autoprefixer')
+  require('postcss-import'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
 ]
 
 module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js'
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
     },
-    mode: 'development',
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                //css loading options for PostCSS url=false prevents images from being bundled
-                use: ['style-loader', 'css-loader?url=false', {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins: postCSSPlugins
-                    }
-                }]
-            }
-        ]
-    }
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+      }
+    ]
+  }
 }
